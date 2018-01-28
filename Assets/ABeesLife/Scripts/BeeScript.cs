92600db125ab9ce24121fa515357a844435047c3
Maria Luisa Carrion D <marilu597@gmail.com>
2018-01-28 12:23:29 -0500

    Update script to add pics to show

--- Assets/ABeesLife/Scripts/BeeScript.cs
+++ Assets/ABeesLife/Scripts/BeeScript.cs
@@ -13,6 +13,9 @@ public class BeeScript : MonoBehaviour {
 	public GameObject uiDie;
 	public Text txtDie;
 	public Text txtPollinatedFlowersCounter;
+	public Image uiPic01;
+	public Image uiPic02;
+	public Image uiPic03;
 
 	public string dieOnGroundMessage = "Fuiste devorado por una araña gigantesca :(";
 
@@ -27,6 +30,11 @@ public class BeeScript : MonoBehaviour {
     private float bellyDecrease = -0.0833333F; //(100 / (20 * 60))
 
 	private int pollinatedFlowersCounter = 0;
+	private bool isAlive = true;
+
+	private bool pic01Seen = true;
+	private bool pic02Seen = false;
+	private bool pic03Seen = false;
 
 
     // Use this for initialization
@@ -36,62 +44,74 @@ public class BeeScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        bellyRadialProgressBar.UpdateAmount(bellyDecrease * Time.deltaTime);
+		if (isAlive) {
+			bellyRadialProgressBar.UpdateAmount(bellyDecrease * Time.deltaTime);
 
-		if (Input.GetButtonDown("Jump")) {
-            flyingEnergyRadialProgressBar.UpdateAmount(flyingEnergyDecrease);
-        } 
-        else if (flyingEnergyRadialProgressBar.GetCurrentAmount() < 100) {
-            //Debug.Log(flyingEnergyIncrease * Time.deltaTime);
-            flyingEnergyRadialProgressBar.UpdateAmount(flyingEnergyIncrease * Time.deltaTime);
-        }
+			if (Input.GetButtonDown("Jump")) {
+				flyingEnergyRadialProgressBar.UpdateAmount(flyingEnergyDecrease);
+			} 
+			else if (flyingEnergyRadialProgressBar.GetCurrentAmount() < 100) {
+				//Debug.Log(flyingEnergyIncrease * Time.deltaTime);
+				flyingEnergyRadialProgressBar.UpdateAmount(flyingEnergyIncrease * Time.deltaTime);
+			}
+		}
 	}
 
 	void Die() {
-		
+		isAlive = false;
 	}
 
 	void ShowEndScreen(string message) {
 		txtDie.text = message;
 		txtDie.text += "\n\nPero polinizaste " + pollinatedFlowersCounter + " flores!\n\nY aquí hay unas lindas fotos de los lugares que visitaste";
 		uiDie.SetActive (true);
+
+		if (pic01Seen) {
+			//uiPic01.SetActive (true);
+		} else if (pic02Seen) {
+			//uiPic02.SetActive (true);
+		} else if (pic03Seen) {
+			//uiPic03.SetActive (true);
+		}
 	}
 
 	void OnTriggerEnter(Collider other) {
-		Debug.Log ("Trigger with: " + other.tag);
+		//Debug.Log ("Trigger with: " + other.tag);
 
-		if (other.CompareTag ("Nectar")) {
-			FlowerScript flowerScript = other.GetComponent<FlowerScript> ();
-			if (flowerScript != null) {
-				flowerScript.takeNectar ();
-				nectarMeter += nectarIncrease;
-                nectarRadialProgressBar.UpdateAmount(nectarIncrease);
+		if (isAlive) {
+			if (other.CompareTag ("Nectar")) {
+				FlowerScript flowerScript = other.GetComponent<FlowerScript> ();
+				if (flowerScript != null) {
+					flowerScript.takeNectar ();
+					nectarMeter += nectarIncrease;
+					nectarRadialProgressBar.UpdateAmount(nectarIncrease);
 
-				if (flowerScript.hasPollen ()) {
-					pollenMeter += pollenIncrease;
-                    pollenRadialProgressBar.UpdateAmount(pollenIncrease);
-				} else {
-					pollenMeter -= pollenIncrease;
+					if (flowerScript.hasPollen ()) {
+						pollenMeter += pollenIncrease;
+						pollenRadialProgressBar.UpdateAmount(pollenIncrease);
+					} else {
+						pollenMeter -= pollenIncrease;
 
-					if (pollenRadialProgressBar.GetCurrentAmount () > 0) {
-						pollenRadialProgressBar.UpdateAmount(-pollenIncrease);
-						pollinatedFlowersCounter++;
-						txtPollinatedFlowersCounter.text = "" + pollinatedFlowersCounter;
+						if (pollenRadialProgressBar.GetCurrentAmount () > 0) {
+							pollenRadialProgressBar.UpdateAmount(-pollenIncrease);
+							pollinatedFlowersCounter++;
+							txtPollinatedFlowersCounter.text = "" + pollinatedFlowersCounter;
+						}
+
 					}
-                    
+					//Debug.Log ("Belly: " + bellyMeter);
+					//Debug.Log ("Nectar: " + nectarMeter);
+					//Debug.Log ("Pollen: " + pollenMeter);
 				}
-				//Debug.Log ("Belly: " + bellyMeter);
-				//Debug.Log ("Nectar: " + nectarMeter);
-				//Debug.Log ("Pollen: " + pollenMeter);
-            }
-            else {
-				Debug.Log ("FlowerScript not set to object with Nectar tag");
+				else {
+					Debug.Log ("FlowerScript not set to object with Nectar tag");
+				}
+			} else if (other.CompareTag ("Respawn")) {
+				// ROGELIO: Fill belly to 100%
+			} else if (other.CompareTag ("Ground")) {
+				Die ();
+				ShowEndScreen (dieOnGroundMessage);
 			}
-		} else if (other.CompareTag ("Respawn")) {
-			// ROGELIO: Fill belly to 100%
-		} else if (other.CompareTag ("Ground")) {
-			Die ();
-			ShowEndScreen (dieOnGroundMessage);
 		}
 	}
 }
